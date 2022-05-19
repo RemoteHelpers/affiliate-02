d4a270390301a612bd621ecf4ac16bca758aa500
alex-voytko <voytko1994@gmail.com>
2022-05-19 15:30:18 +0300

    new start 1.0.

---
+++ assets/js/slick-swiper.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".swiper-wrapper").slick({
+    arrows: false,
+    speed: 500,
+    autoplay: true,
+    touchThreShold: 10,
+    waitForAnimate: false,
+    centerMode: true,
+    variableWidth: true,
+    pauseOnHover: false,
+  });
+});
---
+++ assets/js/swiper.js
@@ -0,0 +1,11 @@
+new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  // loop: true,
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  speed: 800,
+  grabCursor: true,
+});
